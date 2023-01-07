8b0b00a85929806c604c32c5ba815260006a3b72
Yaseen Mohammed <yaseengem@gmail.com>
2023-01-08 08:45:27 +1100

    .Just hello working on cloud. Issue with communicating with the SQL server on cloud. So planing to put it on the VM with firebase emulator.

--- functions/initApp.js
+++ functions/initApp.js
@@ -34,7 +34,7 @@ var passw;
 exports.setEnv = function () {
   if (!process.env.FUNCTIONS_EMULATOR) {
     //DO STUFF SPECIFIC TO EMULATOR ENVIRONMENT
-    host = '34.148.118.236';
+    host = '34.73.121.244';
     port = 33060;
     username = 'yaseen';
     passw = 'Inno12!@';
