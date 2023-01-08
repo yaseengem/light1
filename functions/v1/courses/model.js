3d458ecd67c0c19382f1eba626e7ab6bf3e4193b
Yaseen Mohammed <yaseengem@gmail.com>
2023-01-08 17:28:39 +1100

    .

---
+++ functions/v1/courses/model.js
@@ -0,0 +1,24 @@
+// Plan to use this type of models for later.
+
+
+// class Course {
+//   //...
+//   constructor(name, desc, author, duration) {
+//     this.name = name;
+//     this.desc = desc;
+//     this.author = author;
+//     this.duration = duration;
+//   }
+
+//   durationInHrs() {
+//     return this.duration / 60;
+//   }
+
+// }
+
+// module.exports = Course // 👈 Export class
+
+// Use this class like this
+// const Square = require('./square.js');
+// const mySquare = new Square(2);
+// console.log(`The area of mySquare is ${mySquare.area()}`);
